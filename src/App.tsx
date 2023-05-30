3a6e08741163f12257a5cb5c5551d38282dfdaa3
Dea Alverina <dealverina@gmail.com>
2023-05-30 12:15:45 +0700

    add menu on sidebar

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,124 @@
-import { Layout } from 'antd';
-import React from 'react';
-import type { FC } from 'react';
-import 'antd/dist/reset.css';
-import './App.css';
+import React, { useState } from "react";
+import {
+  AppstoreOutlined,
+  BarChartOutlined,
+  CloudOutlined,
+  HomeOutlined,
+  ShopOutlined,
+  TeamOutlined,
+  UploadOutlined,
+  UserOutlined,
+  VideoCameraOutlined,
+} from "@ant-design/icons";
+import { Layout, Menu, theme } from "antd";
+import type { FC } from "react";
+import "./App.css";
+
+const { Header, Content, Footer, Sider } = Layout;
+
+export interface ItemInterface {
+  key: string;
+  icon?: any;
+  label: string;
+  children?: ItemInterface[];
+}
+
+const items: ItemInterface[] = [
+  {
+    key: "dashboard",
+    icon: React.createElement(HomeOutlined),
+    label: "Dashboard"
+  },
+  {
+    key: "1",
+    icon: React.createElement(UserOutlined),
+    label: "Menu 1",
+    children: [
+      {
+        key: "1-1",
+        label: "Children 1-1",
+        children: [
+          {
+            key: "1-1-1",
+            label: "Children 1-1-1",
+          },
+          {
+            key: "1-1-2",
+            label: "Children 1-1-2",
+          },
+        ],
+      },
+      {
+        key: "1-2",
+        label: "Children 1-2",
+      },
+    ],
+  },
+  { key: "2", icon: React.createElement(VideoCameraOutlined), label: "Menu 2" },
+  { key: "3", icon: React.createElement(UploadOutlined), label: "Menu 3" },
+  { key: "4", icon: React.createElement(BarChartOutlined), label: "Menu 4" },
+  { key: "5", icon: React.createElement(CloudOutlined), label: "Menu 5" },
+  { key: "6", icon: React.createElement(AppstoreOutlined), label: "Menu 6" },
+  { key: "7", icon: React.createElement(TeamOutlined), label: "Menu 7" },
+  { key: "8", icon: React.createElement(ShopOutlined), label: "Menu 8" },
+];
 
 const App: FC = () => {
-  const { Header, Content, Footer, Sider } = Layout;
+  const [collapsed, setCollapsed] = useState(false);
+  const [selectedMenu, setSelectedMenu] = useState("dashboard");
+  const {
+    token: { colorBgContainer },
+  } = theme.useToken();
+
+  const getActiveMenu = () => {
+    let activeMenu = "";
+
+    if (selectedMenu.includes("-")) {
+      console.log(selectedMenu)
+    } else {
+      const item = items.find((val) => val?.key === selectedMenu);
+      if (item) activeMenu = item.label;
+    }
+
+    return activeMenu;
+  };
 
   return (
-    <Layout hasSider>
+    <Layout style={{ minHeight: "100vh" }}>
       <Sider
-        style={{
-          overflow: 'auto',
-          height: '100vh',
-          position: 'fixed',
-          left: 0,
-          top: 0,
-          bottom: 0,
-          color: 'white'
-        }}
+        collapsible
+        collapsed={collapsed}
+        onCollapse={(value) => setCollapsed(value)}
+        width={300}
       >
-        left sidebar
+        <div className="demo-logo-vertical">LOGO</div>
+        <Menu
+          theme="dark"
+          mode="inline"
+          defaultSelectedKeys={[selectedMenu]}
+          items={items}
+          onClick={(e) => setSelectedMenu(e.key)}
+        />
       </Sider>
-      <Layout className="site-layout" style={{ marginLeft: 200 }}>
-        <Header style={{ color: 'white' }}>header</Header>
-        <Content style={{ margin: '24px 16px 0', overflow: 'initial' }}>main content</Content>
-        <Footer style={{ textAlign: 'center' }}>Demo AntD ©2023 Created by Dea Alverina</Footer>
+      <Layout>
+        <Header style={{ padding: "0 0 0 1em", background: colorBgContainer }}>
+          <b>{getActiveMenu().toUpperCase()}</b>
+        </Header>
+        <Content
+          style={{
+            margin: "1em",
+            padding: "1em",
+            background: colorBgContainer,
+          }}
+        >
+          main content
+        </Content>
+        <Footer style={{ textAlign: "center" }}>
+          Demo AntD ©2023 Created by Dea Alverina
+        </Footer>
       </Layout>
     </Layout>
-  )
-}
+  );
+};
 
 export default App;
